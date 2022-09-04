a9ee141247ab3de5719d883ac3c0df471bf2359d
AayushMohan <aayushmohan1702@gmail.com>
2022-09-04 22:29:11 +0530

    Added: Lottery Dapp

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,11 @@ const Header = () => {
       <img
         src="https://avatars.githubusercontent.com/u/66319691?v=4"
         alt="User Image"
-        className="rounded-full "
+        className="rounded-full h-20 w-20"
       />
+      <div>
+        <h1>Lottery DAPP</h1>
+      </div>
     </div>
   );
 };
