afa8573bd029fb828e6ca7315aed82878641129e
AayushMohan <aayushmohan1702@gmail.com>
2022-09-04 22:31:25 +0530

    Added: Flex

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,16 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <img
-        src="https://avatars.githubusercontent.com/u/66319691?v=4"
-        alt="User Image"
-        className="rounded-full h-20 w-20"
-      />
-      <div>
-        <h1>Lottery DAPP</h1>
-        <p>User...</p>
+      <div className="flex">
+        <img
+          src="https://avatars.githubusercontent.com/u/66319691?v=4"
+          alt="User Image"
+          className="rounded-full h-20 w-20"
+        />
+        <div>
+          <h1>Lottery DAPP</h1>
+          <p>User...</p>
+        </div>
       </div>
     </div>
   );
