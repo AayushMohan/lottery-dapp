a08fe7836ae0f400eec4b1b3bf4cf722e5118f5b
AayushMohan <aayushmohan1702@gmail.com>
2022-09-04 20:31:56 +0530

    Added: User Image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img
+        src="https://avatars.githubusercontent.com/u/66319691?v=4"
+        alt="User Image"
+        className="rounded-full "
+      />
+    </div>
+  );
 };
 
 export default Header;
