6fab40934e0f0d92791b0bbd09b1774c8264c749
AayushMohan <aayushmohan1702@gmail.com>
2022-09-05 15:11:53 +0530

    Styled: Header Component

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { Bars3BottomRightIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
-    <div className="grid">
+    <div className="grid grid-cols-2 md:grid-cols-5 justify-between items-center p-5">
       <div className="flex items-center space-x-2">
         <img
           src="https://avatars.githubusercontent.com/u/66319691?v=4"
@@ -16,8 +16,9 @@ const Header = () => {
           <p className="text-xs text-emerald-500 truncate">User...</p>
         </div>
       </div>
-      <div>
-        <div className="bg-[#0A1F1C] p-4">
+
+      <div className="hidden md:flex md:col-span-3 justify-center rounded-md">
+        <div className="bg-[#0A1F1C] p-4 rounded space-x-2">
           <NavButton isActive title="Buy Tickets" />
           <NavButton title="Logout" />
         </div>
