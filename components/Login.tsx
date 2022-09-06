c34e9f7e174c264c619f956865d2a7842035d776
AayushMohan <aayushmohan1702@gmail.com>
2022-09-06 20:58:44 +0530

    Styled: Image

--- components/Login.tsx
+++ components/Login.tsx
@@ -5,9 +5,11 @@ const Login = () => {
     <div>
       <div>
         <img
+          className="rounded-full h-56 w-56 mb-10"
           src="https://avatars.githubusercontent.com/u/66319691?v=4"
           alt="AayushMohan Image"
         />
+        <h1>THE LOTTERY DAPP</h1>
       </div>
     </div>
   );
