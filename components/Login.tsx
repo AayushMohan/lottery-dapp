44b13062bf012fb0d91d86496849bc4de6db363e
AayushMohan <aayushmohan1702@gmail.com>
2022-09-06 20:57:17 +0530

    Added: Login Image

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Woah you need to login!</div>;
+  return (
+    <div>
+      <div>
+        <img
+          src="https://avatars.githubusercontent.com/u/66319691?v=4"
+          alt="AayushMohan Image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
