806d2bbffed55f004b887cb734f178bbc9aefb0a
AayushMohan <aayushmohan1702@gmail.com>
2022-09-13 20:06:28 +0530

    Added: Styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,10 +21,16 @@ const Home: NextPage = () => {
 
   if (isLoading) {
     return (
-      <div>
-        <div>
-          <img src="https://i.imgur.com/4h7mAu7.com" alt="" />
-          <h1>Loading The LOTTERY DAPP</h1>
+      <div className="bg-[#091B18] h-screen flex flex-col items-center justify-center">
+        <div className="flex items-center space-x-2 mb-10">
+          <img
+            src="https://i.imgur.com/4h7mAu7.com"
+            alt=""
+            className="rounded-full h-20 w-20"
+          />
+          <h1 className="text-lg text-white font-bold ">
+            Loading The LOTTERY DAPP
+          </h1>
         </div>
       </div>
     );
